0368960778e46d76995fcda25b09605d02c61131
ryuash <ryuash123@gmail.com>
2019-02-22 14:39:04 -0500

    start

---
+++ app/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import routes from './constants/routes';
+import App from './containers/App';
+import HomePage from './containers/HomePage';
+import CounterPage from './containers/CounterPage';
+import TestPage from './containers/TestPage';
+
+export default () => (
+  <App>
+    <Switch>
+      <Route exact path={routes.HOME} component={HomePage} />
+      <Route path={routes.TEST} component={TestPage} />
+      <Route path={routes.COUNTER} component={CounterPage} />
+    </Switch>
+  </App>
+);
