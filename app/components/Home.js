fe526b027600820f2a8381cb921df06e8b2c594a
ryuash <ryuash123@gmail.com>
2019-02-26 12:52:58 -0500

    split view up and edited modular css file

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,3 +1,23 @@
+import React from 'react';
+import SplitPane from 'react-split-pane';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div id="home-container">
+        <div id="list-notes">Notes</div>
+        <div id="editor">
+          <SplitPane split="vertical" defaultSize="50%">
+            <div className="list-pane" />
+            <div className="editor-pane" />
+            <div className="view-pane" />
+          </SplitPane>
+        </div>
+      </div>
+    );
+  }
+}
+
 // @flow
 // import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
@@ -17,64 +37,64 @@
 //     );
 //   }
 // }
+//=============================================
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import routes from '../constants/routes';
+// import styles from './Home.css';
+// import { connect } from 'react-redux';
+// import { homeTestThunk } from '../actions/noteActions';
 
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
-import styles from './Home.css';
-import { connect } from 'react-redux';
-import { homeTestThunk } from '../actions/homeActions';
+// type Props = {};
 
-type Props = {};
+// class Home extends Component<Props> {
+//   constructor() {
+//     super();
+//     this.state = {
+//       input: ''
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
 
-class Home extends Component<Props> {
-  constructor() {
-    super();
-    this.state = {
-      input: ''
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+//   handleChange(e) {
+//     this.setState({ input: e.target.value });
+//   }
+//   handleClick(e) {
+//     e.preventDefault();
+//     this.props.homeTestThunk(this.state.input);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>IS HOME</h1>
+//         <form onSubmit={this.handleClick}>
+//           <input
+//             value={this.state.input}
+//             onChange={this.handleChange}
+//             placeholder="enter"
+//           />
+//           <button type="submit" onClick={this.handleClick}>
+//             Hit me
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
-  handleChange(e) {
-    this.setState({ input: e.target.value });
-  }
-  handleClick(e) {
-    e.preventDefault();
-    this.props.homeTestThunk(this.state.input);
-  }
-  render() {
-    return (
-      <div>
-        <h1>IS HOME</h1>
-        <form onSubmit={this.handleClick}>
-          <input
-            value={this.state.input}
-            onChange={this.handleChange}
-            placeholder="enter"
-          />
-          <button type="submit" onClick={this.handleClick}>
-            Hit me
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    homeTestThunk: input => {
-      dispatch(homeTestThunk(input));
-    }
-  };
-};
-export default connect(
-  null,
-  mapDispatch
-)(Home);
-
+// const mapDispatch = dispatch => {
+//   return {
+//     homeTestThunk: input => {
+//       dispatch(homeTestThunk(input));
+//     }
+//   };
+// };
+// export default connect(
+//   null,
+//   mapDispatch
+// )(Home);
+//===============================
 // import { bindActionCreators } from 'redux';
 // import { connect } from 'react-redux';
 // import Counter from '../components/Counter';
